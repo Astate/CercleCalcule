686356d5d415264ab969bad57c2b350599532137
Zacharie Roy <zacharieroy@gmail.com>
2017-11-05 22:19:06 -0500

    Remove all findViewById and import kotlinx.android.synthetic.main. Correct diametre formul

--- app/src/main/java/com/ztechnologie/mastarch/cerclecalcule/MainActivity.kt
+++ app/src/main/java/com/ztechnologie/mastarch/cerclecalcule/MainActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -21,76 +22,43 @@ class MainActivity : AppCompatActivity() {
         return str.toDoubleOrNull()
     }
 
-    @SuppressLint("SetTextI18n")
-    fun onAnswerClick(view: View) {
-
-        val answer = (findViewById<EditText>(R.id.editText_result) as EditText).text.toString()
-        val spinner = findViewById<View>(R.id.spinner) as Spinner
-
-
-
-        //var rayon = java.lang.Double.parseDouble(answer)
-        var rayon = parseDouble(answer)
-        if (rayon == null) {
-            println("Mauvais format'$rayon'")
-            return
-        }
-
-        if (spinner.getSelectedItem() == "Diamètre"){
-            rayon = rayon *2
-        }
-
-
-        if (rayon < 0) {
-            val toasty = Toast.makeText(applicationContext, "Ce nombre est négatif !", Toast.LENGTH_SHORT)
-            toasty.show()
-        } else if (rayon > 0) {
-            val c = Calculateurs()
-            val tc = findViewById<TextView>(R.id.cire) as TextView
-            tc.text = "" + c.circonference(rayon)
-            val ta = findViewById<TextView>(R.id.aire) as TextView
-            ta.text = "" + c.aire(rayon)
-        } else {
-            val toasty = Toast.makeText(applicationContext, "Ce nombre est nul !", Toast.LENGTH_SHORT)
-            toasty.show()
-        }
-
-
-    }
-
-
-   // lateinit var editText2: EditText
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val spinner = findViewById<Spinner>(R.id.spinner) as Spinner
-   //     editText2 = findViewById<EditText>(R.id.editText2) as EditText
-        // Create an ArrayAdapter using the string array and a default spinner layout
         val adapter = ArrayAdapter.createFromResource(this,
                 R.array.mesure_array, android.R.layout.simple_spinner_item)
         // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         // Apply the adapter to the spinner
         spinner.adapter = adapter
+    }
 
-    }
-/*
-    override fun onResume() {
-        super.onResume()
-        editText2.setOnEditorActionListener( { textView, action, event ->
-            var handled = false
-            if (action == EditorInfo.IME_ACTION_DONE) {
-                println("------------------------------Tu viens de cliquer sur OK-----------------------------------------")
-                var crotte = onAnswerClick()
-                println(crotte)
-                handled = true
+    @SuppressLint("SetTextI18n")
+
+    fun onAnswerClick(view: View) {
+
+        val answer = editText_result.text.toString()
+        var rayon = parseDouble(answer)
+
+
+        if (rayon == null) {
+            val toasty = Toast.makeText(applicationContext, "Ce nombre est nul !", Toast.LENGTH_SHORT)
+            toasty.show()
+            return
+        }
+        if (rayon < 0) {
+            val toasty = Toast.makeText(applicationContext, "Ce nombre est négatif !", Toast.LENGTH_SHORT)
+            toasty.show()
+        } else if (rayon > 0) {
+            val c = Calculateurs()
+            if (spinner.getSelectedItem() == "Diamètre"){
+                rayon = rayon / 2.0
             }
-            handled
-        })
+            cire.text = c.circonference(rayon).toString()
+            aire.text = c.aire(rayon).toString()
+        }
     }
-*/
+
 }
 
 
